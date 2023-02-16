f68b76ac6896e0c525675db6f01620744eccafa2
Gustavo Alfredo Correa Da Silva <111892331+Gustavocorrea0@users.noreply.github.com>
2023-02-15 21:35:41 -0300

    Update and rename CalculoDeNotas.java to Exercicio3.java

--- Exercicio3.java
+++ Exercicio3.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class CalculoDeNotas {
+public class Exercicio3 {
     static Scanner capturaDeDados = new Scanner(System.in);
 
     public static void main(String[] args) {
